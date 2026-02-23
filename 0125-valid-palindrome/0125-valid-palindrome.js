3d01bd3ff4e132f3fe56cf95c07fc9bf381975d7
Aditya Jadhav <adijadhav031@gmail.com>
2026-02-23 19:32:46 +0530

    Time: 30 ms (5.03%), Space: 62 MB (17.28%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -6,7 +6,7 @@ var isPalindrome = function(s) {
     let str = s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase().split('');
     console.log('str',str)
     let l=0,r=str.length-1;
-    for(let i=0;i<str.length;i++){
+    while(l<r){
         if(str[l]!=str[r]){
             return false;
         }
